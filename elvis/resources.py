a02cb16de13b21c2ed6a5aa2da70e3d5863b7478
donkeysharp <serguimant@gmail.com>
2016-06-14 18:58:26 -0400

    Now template resource executes in remote host

--- elvis/resources.py
+++ elvis/resources.py
@@ -1,4 +1,8 @@
 import os
+import time
+from fabric.api import *
+from fabric.tasks import execute
+
 
 class actions(object):
     CREATE      = 'create'
@@ -10,18 +14,31 @@ class actions(object):
     RESTART     = 'restart'
     RELOAD      = 'reload'
 
+
+def __save_temporal_file(content):
+    if not os.path.exists('./.__tmp__') or not os.path.isdir('./.__tmp__'):
+        os.mkdir('./.__tmp__')
+    random_name = str(time.time())
+    f = open('./.__tmp__/%s' % random_name, 'w')
+    f.write(content)
+    f.write('\n')
+    f.close()
+
+    return './.__tmp__/' + random_name
+
+
 def template(peanut, destination, options={}):
-    """
-    template(peanut, '/dest/folder', {
-        'source': 'sometemplate.tpl'
-    })
-    """
-    env = peanut.template_env
-    template = env.get_template(options['source'])
+    tplenv = peanut.template_env
+    template = tplenv.get_template(options['source'])
 
     variables = {}
     if options.has_key('variables'):
         variables = options['variables']
-
     content = template.render(**variables)
-    print(content)
+
+    filename = __save_temporal_file(content)
+
+    def remote_run():
+        put(filename, destination)
+
+    execute(remote_run, hosts=['donkeysharp@192.168.56.120'])
